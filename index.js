c4a38f2c283daed301bb93122fa00357dbf22bf3
GeyzenJ <jangeyzen@hotmail.com>
2024-10-21 12:29:53 +0200

    Added API artist endpoint

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ app.get('/', (req, res) => {
     res.send("Hello world");
 });
 
+app.get('/api/artists', (req, res) => {
+    res.send([
+        "JB",
+        "Beyonce",
+    ]);
+});
+
 // Starten van de server en op welke poort de server moet luisteren
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
