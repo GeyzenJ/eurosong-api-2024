8493c65f2bfa128f1ceb0020490071571ad60121
GeyzenJ <jangeyzen@hotmail.com>
2024-10-21 12:09:52 +0200

    Express init

---
+++ index.js
@@ -0,0 +1,14 @@
+// importeren van de express module in node_modules
+const express = require('express');
+// aanmaken van een express app
+const app = express();
+
+// endpoints
+app.get('/', (req, res) => {
+    res.send("Hello world");
+});
+
+// Starten van de server en op welke poort de server moet luisteren
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
