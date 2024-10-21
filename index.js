fa56544fd547a51de3bef7d25570a98f5af5789a
GeyzenJ <jangeyzen@hotmail.com>
2024-10-21 13:59:15 +0200

    Added database class

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 // importeren van de express module in node_modules
 const express = require('express');
+const mysql = require('mysql2/promise');
+
 // aanmaken van een express app
 const app = express();
+
+// Create the connection to database
+const connect = async () => {
+   const connection = await mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'eurosongdb',
+    port: 3306,
+  });
+
+  const [rows ] = await connection.execute('SELECT * FROM artists');
+  console.log(rows);
+}
+connect();
 
 // endpoints
 app.get('/', (req, res) => {
